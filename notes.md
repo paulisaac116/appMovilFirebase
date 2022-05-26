095c3ba28b59da3cb50a4a0375f73cb8a63af49c
isaacdev <paulgualab@gmail.com>
2022-05-25 19:34:53 -0500

    Authentication first implementation

---
+++ notes.md
@@ -0,0 +1,52 @@
+## Commands
+<hr>
+
+### Services  
+Create a new service into the <code>Services</code> directory
+```
+ionic g service servieces/avatar
+```
+<hr>
+
+### Installations  
+
+Install the necessary dependencies to use the camera function
+```
+npm i @capacitor/camere
+```
+
+
+Allows to use the camera function in the project
+
+
+```
+npm i @ionic/pwa-elements
+```
+
+Install the angula clients
+```
+npm install -g @angular/cli
+```
+
+Install the necessary firebase dependencies
+
+```
+npm i firebase add @angular/fire
+```
+
+Start the authentication Firebase service
+```
+ng add @angular/fire
+```
+Another option to start the client authentication:
+```
+firebase login --reauth
+```
+
+Services to install:
+* Cloud Storage
+* Authentication
+* Firebase
+<hr>
+
+
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+import {redirectUnauthorizedTo, redirectLoggedInTo, canActivate} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
+const routes: Routes = [
+  
+  {
+    path: '',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    ...canActivate(redirectLoggedInToHome)
+  },
+  
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  // },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
